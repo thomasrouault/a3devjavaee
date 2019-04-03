7b9d371850abbd99dccf113e562828432653da87
Thomas Rouault <throuault44@gmail.com>
2019-04-03 12:10:01 +0200

    add bug

---
+++ src/main/java/fr/imie/contact/entities/DateUtils.java
@@ -0,0 +1,17 @@
+package fr.imie.contact.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+
+    public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static LocalDate toLocalDate(String text){
+        if (text == null || text.isEmpty()) {
+            return null;
+        }
+        LocalDate date = LocalDate.parse(text, formatter);
+        return date;
+    }
+}
--- src/main/java/fr/imie/contact/servlets/PersonServlet.java
+++ src/main/java/fr/imie/contact/servlets/PersonServlet.java
@@ -1,13 +1,18 @@
 package fr.imie.contact.servlets;
 
+import fr.imie.contact.entities.DateUtils;
 import fr.imie.contact.entities.Person;
 import fr.imie.contact.repositories.PersonRepository;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
-import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @WebServlet("/person/*")
@@ -19,7 +24,18 @@ public class PersonServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        repository.save(new Person());
+        if (request.getMethod().equalsIgnoreCase("post")){
+            Person person = new Person();
+            person.setFirstName(request.getParameter("firstName"));
+            person.setLastName(request.getParameter("lastName"));
+            person.setEmail(request.getParameter("email"));
+
+            String text = request.getParameter("birthDate");
+            LocalDate date = DateUtils.toLocalDate(text);
+            person.setBirthDate(date);
+
+            repository.save(person);
+        }
 
         List<Person> persons = repository.findAll();
         request.setAttribute("persons", persons);
